13d35f93043e40b269bc2173bceea689c38d56bb
Nate <nate@qstreet.org>
2025-05-05 18:19:19 -0700

    work on rust side

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,11 +1,19 @@
+use std::process::Command;
+
 #[tauri::command]
 fn greet(name: &str) -> String {
     format!("Hello, {}!", name)
 }
 
+#[tauri::command]
+fn start(white_path: &str, black_path: &str) {
+    let white = Command::new(white_path).spawn().unwrap();
+    let black = Command::new(black_path).spawn().unwrap();
+}
+
 pub fn run() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![greet, start])
         .run(tauri::generate_context!())
         .expect("Error while running Tauri");
 }
--- src/app.tsx
+++ src/app.tsx
@@ -1,52 +1,79 @@
 import { invoke } from "@tauri-apps/api/core"
 import { Chess, Square } from "chess.js"
+import { useState } from "react"
 
 const size = 80
 const white = "#f0d9b5"
 const black = "#b58863"
 
 export default function App() {
+  const [started, setStarted] = useState(false)
+  const [whitePath, setWhitePath] = useState("")
+  const [blackPath, setBlackPath] = useState("")
+
   const chess = new Chess()
 
-  const x = async () => {
-    console.log(await invoke("greet", { name: "Nate" }))
-  }
-
-  x()
+  const files = ["a", "b", "c", "d", "e", "f", "g", "h"]
+  const ranks = [8, 7, 6, 5, 4, 3, 2, 1]
 
   return (
     <>
       <h1>Rookpi</h1>
 
-      {Array.from({ length: 8 }).map((_, i) => (
-        <div key={i} style={{ display: "flex" }}>
-          {Array.from({ length: 8 }).map((_, j) => {
-            const square = String.fromCharCode(97 + j) + (8 - i)
-            const piece = chess.get(square as Square)
+      {started ? (
+        <>
+          <button>Start</button>
 
+          {ranks.map((rank, i) => {
             return (
-              <div
-                key={j}
-                style={{
-                  width: size,
-                  height: size,
-                  backgroundColor: (i + j) % 2 === 0 ? white : black,
-                  display: "flex",
-                  justifyContent: "center",
-                  alignItems: "center",
-                }}
-              >
-                {piece && (
-                  <img
-                    src={`pieces/${piece.color}${piece.type}.svg`}
-                    style={{ width: size, height: size }}
-                  />
-                )}
+              <div key={rank} style={{ display: "flex" }}>
+                {files.map((file, j) => {
+                  const piece = chess.get((file + rank) as Square)
+
+                  return (
+                    <div
+                      key={file}
+                      style={{
+                        width: size,
+                        height: size,
+                        backgroundColor: (i + j) % 2 === 0 ? white : black,
+                      }}
+                    >
+                      {piece && (
+                        <img
+                          src={`pieces/${piece.color}${piece.type}.svg`}
+                          style={{ width: size, height: size }}
+                        />
+                      )}
+                    </div>
+                  )
+                })}
               </div>
             )
           })}
-        </div>
-      ))}
+        </>
+      ) : (
+        <>
+          <input
+            value={whitePath}
+            onChange={(e) => setWhitePath(e.target.value)}
+          />
+
+          <input
+            value={blackPath}
+            onChange={(e) => setBlackPath(e.target.value)}
+          />
+
+          <button
+            onClick={async () => {
+              await invoke("start", { whitePath, blackPath })
+              setStarted(true)
+            }}
+          >
+            Start
+          </button>
+        </>
+      )}
     </>
   )
 }
